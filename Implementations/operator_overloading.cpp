769b2bcbe816665dbc97efa2703dca2e7d336f32
alinmaslov55 <maslovalin55@gmail.com>
2024-11-03 21:36:40 +0200

    implemented get and set

--- Implementations/operator_overloading.cpp
+++ Implementations/operator_overloading.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-template<typename T, int n, int m>
+template<typename T, int n, int m, typename = std::enable_if_t<std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>>>
 class Matrix{
     int rows = n;
     int cols = m;
     T** data;
     public:
-    template <typename U = T, typename = std::enable_if_t<std::is_same_v<U, int> || std::is_same_v<U, float> || std::is_same_v<U, double>>>
     Matrix(){
         data = new T*[rows];
         for(int i = 0; i < rows; ++i){
@@ -25,7 +24,20 @@ class Matrix{
             }
         }
     }
-
+    void set(const int &krow, const int &kcol, const T & value){
+        if(krow <= row - 1 && kcol <= column - 1 && krow >= 0 && kcol >= 0)
+        data[krow][kcol] = value;
+        else{
+            std::cerr << "Invalid index" << std::endl;
+        }
+    }
+    T get(const int &krow, const int &kcol, const T & value){
+        if(krow <= row - 1 && kcol <= column - 1 && krow >= 0 && kcol >= 0)
+        return data[krow][kcol];
+        else{
+            std::cerr << "Invalid index" << std::endl;
+        }
+    }
     ~Matrix(){
         for(auto row : data){
             delete[] row;
@@ -35,6 +47,6 @@ class Matrix{
 };
 
 int main(){
-    Matrix<double, 3, 4> matrix;
+    Matrix<double, 3, 4> matrix(1);
     return 0;
 }
